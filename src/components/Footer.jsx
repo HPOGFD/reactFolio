6505e17f6fa50ec7f1a08cef4e426d6b16b1c44f
Harry Oyarvide <poyarvide87@yahoo.com>
2024-12-28 07:13:46 -0500

    sepearted css and jsx from the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import '../style/footer.css';
 
 const Footer = () => {
   return (
-    <footer style={{ textAlign: 'center', padding: '1rem', backgroundColor: '#333', color: '#fff' }}>
+    <footer className="footer">
       <p>© {new Date().getFullYear()} Your Portfolio. All Rights Reserved.</p>
-      <div>
+      <div className="footer-links">
         <a
           href="https://github.com/HPOGFD"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ margin: '0 1rem', color: '#fff' }}
+          className="footer-link"
         >
           <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
@@ -19,7 +20,7 @@ const Footer = () => {
           href="https://linkedin.com/in/harry-oyarvide-25a1585a/"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ margin: '0 1rem', color: '#fff' }}
+          className="footer-link"
         >
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
