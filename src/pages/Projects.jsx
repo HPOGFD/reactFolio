4d8b5cdf2dd5e8cfe9e807de12a7a165be206ebf
Harry Oyarvide <poyarvide87@yahoo.com>
2024-12-28 06:58:07 -0500

    projects tab implemented

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,41 @@
+import '../style/Projects.css';
+
+
 export default function Projects() {
     console.log('About.jsx is running');
     return (
-        <div>
-            <h1>Projects</h1>
-            <p>
-                Projects will be displayed here
-            </p>
+        <div id="projects">
+        <div class="container">
+          <h1 class="sub-title">My Projects</h1>
+          <div class="project-list">
+            <div class="project-item">
+              <i></i>
+              <h2>Budget Web Application</h2>
+              <p>User-friendly application designed to help individuals manage their finances efficiently.</p>
+              <a href="https://github.com/HPOpersonalProjects/BudgetApp.git">Learn More</a>
+            </div>
+            <div class="project-item">
+              <i></i>
+              <h2>Shopping Web Application</h2>
+              <p>It allows users to browse products, view detailed descriptions, add items to their cart, and securely complete purchases.</p>
+              <a href="https://github.com/HPOpersonalProjects/ShoppingApp.git">Learn More</a>
+            </div>
+            <div class="project-item">
+              <i></i>
+              <h2>Shopping Web Application</h2>
+              <p>It allows users to browse products, view detailed descriptions, add items to their cart, and securely complete purchases.</p>
+              <a href="https://github.com/HPOpersonalProjects/ShoppingApp.git">Learn More</a>
+            </div>
+            <div class="project-item">
+              <i></i>
+              <h2>Bored API</h2>
+              <p>Fun, interactive tool that suggests activities to help users combat boredom.</p>
+              <a href="https://github.com/HPOGFD/BoredAPI.git">Learn More</a>
+              
+            </div>
+          </div>
         </div>
+      </div>
+      
     );
 }
